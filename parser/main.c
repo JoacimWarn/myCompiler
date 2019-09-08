5795771d1809d270ff5cf9cd3e83dee7e54e56ff
JoacimWarn <47356924+JoacimWarn@users.noreply.github.com>
2019-09-08 11:20:37 +0200

    Update main.c

--- parser/main.c
+++ parser/main.c
@@ -2,29 +2,30 @@
 #include <stdlib.h>
 #include "lexer.h"
 
-#define BUFFER 512
+#include <time.h>
+
+#define BUFFER 100
 
 #define is_digit(c) ('0' <= c && c <= '9')
 #define ushort unsigned short int
-
-char* infixToPostfixReverse(char*);
-char* buildExpression(char*);
 
 struct tree{
 	
 	struct tree* left;
 	struct tree* right;
 	
+	int level;
 	
-	
+	struct statement* stmt;
 	struct expression* exp;
 	
 };
 
 struct statement{
 	
-	struct expression* condition;
-	char* body;
+	struct statement* next;
+	
+	struct expression* exp;
 	
 };
 
@@ -39,65 +40,292 @@ struct expression{
 	
 };
 
-void checkOperands(struct expression* temp){
+struct tree* addNewNode(struct tree*, struct expression*, int);
+struct expression* addNewElement(struct expression*, char);
+void printBottomUpTree(struct tree*);
+
+char* infixToPostfixReverse(char*);
+char* buildExpression(char*);
+void printBottomUp(struct expression*);
+void checkOperands(struct expression*);
+int isKeyWord(char*);
+int str_cmp(char*, char*);
+
+//adds a new node to the tree struct
+struct tree* addNewNode(struct tree* temp, struct expression* expr, int tab){
 	
-	if(temp == NULL) return;
-	
-	checkOperands(temp->right);
-	
-	checkOperands(temp->left);
-	
-	if(temp->type == 'P'){
+	if(temp == NULL){
 		
-		if(temp->right != 0){
+		temp = (struct tree*) malloc(sizeof(struct tree));
+		
+		temp->exp = expr;
+		temp->level = tab;
+		temp->stmt = NULL;
+		temp->left  = NULL;
+		temp->right = NULL;
+		
+	}
+	else{
+		
+		if(temp->right == NULL){
 			
-			if(temp->left != 0){
+			tab++;
+			temp->right = addNewNode(temp->right, expr, tab);
+			
+		}
+		else if(temp->left == NULL){
+			
+			tab++;
+			temp->left = addNewNode(temp->left, expr, tab);
+			
+		}
+		else{
+			
+			//if random -> 0 then go left, else go right
+			
+			int random = (rand() % (1 - 0 + 1)) + 0;
+			
+			if(random < 1){
 				
-				if((temp->right->type == 'D' || temp->right->type == 'I' || temp->right->operands == '1') && (temp->left->type == 'D' || temp->left->type == 'I' || temp->left->operands == '1')) temp->operands = '1';
+				tab++;
+				temp->left = addNewNode(temp->left, expr, tab);
+				
+			}
+			else{
+				
+				tab++;
+				temp->right = addNewNode(temp->right, expr, tab);
 				
 			}
 			
 		}
 		
 	}
-	/*
-	if(temp->type == 'P'){
+	
+	return temp;
+	
+}
+
+void printBottomUpTree(struct tree* temp){
+	
+	if(temp == NULL) return;
+	
+	printBottomUpTree(temp->right);
+	
+	printBottomUpTree(temp->left);
+	
+	printBottomUp(temp->exp);
+	
+}
+
+
+
+
+
+void printExpressionToFile(FILE* fpOut, struct expression* temp, int tab){
+	
+	if(temp == NULL) return;
+	
+	printExpressionToFile(fpOut, temp->right, tab);
+	
+	printExpressionToFile(fpOut, temp->left, tab);
+	
+	int i;
+	
+	for(i = 0; i < tab; i++) fprintf(fpOut, "\t");
+	
+	fprintf(fpOut, "op:%c val:%c type:%c\n", temp->operator, temp->value, temp->type);
+	
+}
+
+void printTreeToFile(FILE* fpOut, struct tree* temp){
+	
+	//varje nivå vi går ner -->> tab++
+	
+	if(temp == NULL) return;
+	
+	printTreeToFile(fpOut, temp->right);
+	
+	printTreeToFile(fpOut, temp->left);
+	
+	fprintf(fpOut, "\n");
+	
+	printExpressionToFile(fpOut, temp->exp, temp->level);
+	
+}
+
+
+
+
+
+
+int main(){
+	
+	int tab = 0;
+	tab = tab + '\t';
+	
+	printf("hej %c hej", tab );
+	
+	srand(time(0));
+	
+	FILE* fpRead;
+	FILE* fpWrite;
+	int ch, i;
+	char buffer[BUFFER];
+	
+	i =  0;
+	
+	fpRead = fopen("stmt.txt", "r");
+	
+	if(fpRead == NULL){
 		
-		if(temp->right != 0 && temp->left != 0){
+		printf("Error opening file\n");
+		return 1;
+		
+	}
+	
+	fpWrite = fopen("outputParser.txt", "w");
+	
+	if(fpWrite == NULL){
+		
+		printf("Error opening file\n");
+		fclose(fpRead);
+		return 1;
+		
+	}
+	
+	/*
+	while((ch = fgetc(fpRead)) != EOF){
+		
+		fprintf(fpWrite, "%c", ch);
+		printf("%c", ch);
+		
+		if(ch == SEMI_COLON){
 			
-			if(temp->right->type == 'D' && temp->left->type == 'D'){
-				
-				temp->operands = '1';
-				
-			}
-			else if((temp->right->type == 'D' || temp->right->type == 'P') && (temp->left != 0)){
-				
-				temp->operands = '1';
-				
-			}
+			buffer[i] = END_OF_FILE;
+			i = 0;
 			
 		}
+		
+		buffer[i++] = ch;
 		
 	}
 	*/
 	
+	struct expression* root = NULL;
+	root = addNewElement(root, '=');
+	root = addNewElement(root, '+');
+	root = addNewElement(root, '/');
+	root = addNewElement(root, '-');
+	
+	root = addNewElement(root, '1');
+	root = addNewElement(root, '3');
+	checkOperands(root);
+	root = addNewElement(root, '2');
+	checkOperands(root);
+	root = addNewElement(root, 'a');
+	checkOperands(root);
+	root = addNewElement(root, 'a');
+	
+	//printBottomUp(root);
+	
+	struct expression* test = NULL;
+	test = addNewElement(test, '=');
+	test = addNewElement(test, '+');
+	
+	test = addNewElement(test, '1');
+	test = addNewElement(test, '/');
+	
+	test = addNewElement(test, '2');
+	test = addNewElement(test, '*');
+	
+	test = addNewElement(test, '3');
+	test = addNewElement(test, 'c');
+	checkOperands(test);
+	test = addNewElement(test, 'c');
+	checkOperands(test);
+	
+	//printBottomUp(test);
+	
+	struct tree* theTree = NULL;
+	
+	//theTree = addNewNode(theTree, test);
+	
+	//printBottomUp(theTree->exp);
+	
+	for(i = 0; i < 10; i++){
+		
+		theTree = addNewNode(theTree, root, 1);
+		
+	}
+	
+	printTreeToFile(fpWrite, theTree);
+	
+	fclose(fpRead);
+	fclose(fpWrite);
+	
+	//printBottomUpTree(theTree);
+	
+	//printBottomUp(theTree->exp);
+	
+	//char* a = "a = a + 2 / (3-1)\0";
+	//char* theExpression;
+	//theExpression = buildExpression(a);
+	//printf("%s", theExpression);
+	//evaluateExpression(expression);
+	//root->exprPtr = buildExpression(root->exprPtr, a);
+	
+	printf("\nPress any key to terminate...");
+	getch();
+	
+	return 0;
+	
 }
 
-void printBottomUp(struct expression* temp){
+//return 1 if buffer is a keyword
+int isKeyWord(char* buffer){
 	
-	if(temp == NULL) return;
+	char keywords[32][10] = {"auto","break","case","char","const","continue","default",
+		"do","double","else","enum","extern","float","for","goto",
+		"if","int","long","register","return","short","signed",
+		"sizeof","static","struct","switch","typedef","union",
+		"unsigned","void","volatile","while"};
 	
-	printBottomUp(temp->right);
+	int i, flag;
+	flag = 0;
 	
-	printBottomUp(temp->left);
+	for(i = 0; i < 32; i++){
+		
+		if(str_cmp(keywords[i], buffer) == 0){
+			
+			flag = 1;
+			break;
+			
+		}
+		
+	}
 	
-	printf("op:%c val:%c type:%c\n", temp->operator, temp->value, temp->type);
+	return flag;
+	
+}
+
+//return 0 if same word
+int str_cmp(char* s, char* t){
+	
+	while(*s == *t){
+		
+		if(*s == '\0' && *t == '\0') return 0;
+		
+		*s++;
+		*t++;
+		
+	}
+	
+	return 1;
 	
 }
 
 struct expression* addNewElement(struct expression* temp, char data){
-	
-	int static nrOfElements = 0;
 	
 	if(temp == NULL){
 		
@@ -127,8 +355,6 @@ struct expression* addNewElement(struct expression* temp, char data){
 		temp->left = NULL;
 		temp->right = NULL;
 		
-		nrOfElements++;
-		
 	}
 	else{
 		
@@ -138,14 +364,14 @@ struct expression* addNewElement(struct expression* temp, char data){
 			else{
 				
 				if(temp->right->type == 'D' && temp->left == 0){
-					printf("going left:%c\n", data);
+					
 					temp->left = addNewElement(temp->left, data);
 					
 				}
 				else if(temp->right->type == 'D' && temp->left != 0){
 					
 					if(temp->left->type == 'P' && temp->left->operands == '0'){
-						printf("going left:%c\n", data);
+						
 						temp->left = addNewElement(temp->left, data);
 						
 					}
@@ -166,7 +392,7 @@ struct expression* addNewElement(struct expression* temp, char data){
 			if(temp->type == 'P' && temp->right != 0){
 				
 				if(temp->right->type == 'D'){
-					printf("left:%c\n", data);
+					
 					temp->left = addNewElement(temp->left, data);
 					
 				}
@@ -176,16 +402,14 @@ struct expression* addNewElement(struct expression* temp, char data){
 					
 				}
 				else if(temp->right->type == 'P' && temp->right->operands == '1'){
-					printf("left:%c\n", data);
+					
 					temp->left = addNewElement(temp->left, data);
 					
 				}
 				
-				//temp->right = addNewElement(temp->right, data);
-				
 			}
 			else if(temp->type == 'P' && temp->right == 0){
-				printf("right:%c\n", data);
+				
 				//right node is empty, allocate it
 				temp->right = addNewElement(temp->right, data);
 				
@@ -194,175 +418,48 @@ struct expression* addNewElement(struct expression* temp, char data){
 		}
 		
 	}
-	/*
-	else{
-		
-		//expression tree is not empty, look for empty space
-		//if it is a operator then traverse right till you find null space then allocate
-		if(!isalnum(data) && !isdigit(data)) temp->right = addNewElement(temp->right, data);		//operator
-		else{
-			
-			//digit or identifier
-			if(temp->type == 'P' && temp->right != 0){
-				
-				if(temp->right->type == 'P' && temp->right->operands == '1'){
-					
-					if(temp->left == 0){
-						
-						temp->left = addNewElement(temp->left, data);
-						
-					}
-					
-				}
-				else if(temp->right->type == 'P'){
-					
-					printf("right:%c\n", data);
-					temp->right = addNewElement(temp->right, data);
-					
-				}
-				else if(temp->right->type == 'D'){
-					
-					if(temp->left == NULL){
-						
-						printf("left:%c\n", data);
-						temp->left = addNewElement(temp->left, data);
-						
-					}
-					else if(temp->right != 0 && temp->left != 0){
-						
-						//reach bottom of the tree, need to traverse again
-						printf("hej123\n");
-						
-					}
-					
-				}
-				
-			}
-			else if(temp->type == 'P' && temp->right == 0){
-				
-				printf("right:%c\n", data);
-				temp->right = addNewElement(temp->right, data);
-				
-			}
-			else if(temp->type == 'P' && temp->left == 0){
-				
-				printf("left:%c\n", data);
-				temp->left = addNewElement(temp->left, data);
-				
-			}
-			
-		}
-		
-	}
-	*/
 	
 	return temp;
 	
 }
 
-int main(){
+void checkOperands(struct expression* temp){
 	
-	struct tree* program = NULL;
+	if(temp == NULL) return;
 	
-	/*
-	struct expression* root = NULL;
-	root = addNewElement(root, '=');
-	root = addNewElement(root, '+');
-	root = addNewElement(root, '/');
-	root = addNewElement(root, '-');
+	checkOperands(temp->right);
 	
-	root = addNewElement(root, '1');
-	root = addNewElement(root, '3');
-	checkOperands(root);
-	root = addNewElement(root, '2');
-	checkOperands(root);
-	root = addNewElement(root, 'a');
-	checkOperands(root);
-	root = addNewElement(root, 'a');
+	checkOperands(temp->left);
 	
-	printBottomUp(root);
-	
-	
-	
-	struct expression* test = NULL;
-	test = addNewElement(test, '=');
-	test = addNewElement(test, '+');
-	
-	test = addNewElement(test, '1');
-	test = addNewElement(test, '/');
-	
-	test = addNewElement(test, '2');
-	test = addNewElement(test, '*');
-	
-	test = addNewElement(test, '3');
-	test = addNewElement(test, 'c');
-	checkOperands(test);
-	test = addNewElement(test, 'c');
-	checkOperands(test);
-	
-	printBottomUp(test);
-	*/
-	
-	//char* a = "a = a + 2 / (3-1)\0";
-	//char* theExpression;
-	//theExpression = buildExpression(a);
-	//printf("%s", theExpression);
-	//evaluateExpression(expression);
-	//root->exprPtr = buildExpression(root->exprPtr, a);
-	
-	
-	
-	FILE* fpRead;
-	FILE* fpWrite;
-	int ch, i;
-	char buffer[BUFFER];
-	
-	i = 0;
-	
-	fpRead = fopen("outputLexer.txt", "r");
-	
-	if(fpRead == NULL){
+	if(temp->type == 'P'){
 		
-		printf("Error opening file\n");
-		return 1;
-		
-	}
-	
-	fpWrite = fopen("outputParser.txt", "w");
-	
-	if(fpWrite == NULL){
-		
-		printf("Error opening file\n");
-		fclose(fpRead);
-		return 1;
-		
-	}
-	
-	while((ch = fgetc(fpRead)) != EOF){
-		
-		//printf("%c", ch);
-		
-		//if(buffer[0] == IDENTIFIER);
-		
-		if(ch == SEMI_COLON){
+		if(temp->right != 0){
 			
-			buffer[i] = END_OF_FILE;
-			i = 0;
-			
-			//printf("buffer:\n%s", buffer);
+			if(temp->left != 0){
+				
+				if((temp->right->type == 'D' || temp->right->type == 'I' || temp->right->operands == '1') && (temp->left->type == 'D' || temp->left->type == 'I' || temp->left->operands == '1')) temp->operands = '1';
+				
+			}
 			
 		}
 		
-		buffer[i++] = ch;
-		
 	}
 	
-	fclose(fpRead);
-	//fclose(fpWrite);
+}
+
+/* Prints out tree structure from bottom up */
+void printBottomUp(struct expression* temp){
 	
-	return 0;
+	if(temp == NULL) return;
+	
+	printBottomUp(temp->right);
+	
+	printBottomUp(temp->left);
+	
+	printf("op:%c val:%c type:%c\n", temp->operator, temp->value, temp->type);
 	
 }
+
 /* returns expression in postfix and also reverses the expression */
 char* infixToPostfixReverse(char* buffer){
 	
@@ -470,159 +567,5 @@ char* infixToPostfixReverse(char* buffer){
 	
 	//printf("leftStack: %s\n", leftStack);
 	//printf("rightStack: %s\n", rightStack);
-	
-}
-
-char* buildExpression(char* buffer){
-	
-	char* startPtr = buffer;
-	
-	//a=a+2/(3-1)\0
-	ushort size = 50;
-	ushort i = 0;
-	//char buf[size];
-	char buf[size];
-	
-	while(*buffer){
-		
-		//copy contents of buffer in to buf, and remove all white spaces
-		
-		if(!isspace(*buffer)){
-			
-			buf[i] = *buffer;
-			i++;
-			buf[i] = END_OF_FILE;
-			
-		}
-		
-		*buffer++;
-		
-	}
-	
-	infixToPostfixReverse(buf);
-	
-	//printf("%s", buf);
-	
-	int j = 0;
-	
-	
-	//build a tree from the expression
-	struct expression* test = (struct expression*) malloc(sizeof(struct expression));
-	i = 0;
-	int level = 1;
-	
-	while(buf[i] != END_OF_FILE){
-		
-		if(buf[i] == PLUS || buf[i] == MINUS || buf[i] == TIMES || buf[i] == DIV || buf[i] == ASSIGN){
-			//printf("%c", buf[i]);
-			//operator
-			if(i == 0){
-				
-				test->operator = buf[i];
-				test->type = 'P';
-				
-			}
-			else if(i > 0){
-				
-				if(test->right->operator != ASSIGN && test->right->operator != TIMES && test->right->operator != DIV && test->right->operator != MINUS && test->right->operator != PLUS){
-					
-					test->right = (struct expression*) malloc(sizeof(struct expression));
-					test->right->operator = buf[i];
-					test->right->type = 'P';
-					level = 2;
-					
-				}
-				else if(test->right->right->operator != ASSIGN && test->right->right->operator != TIMES && test->right->right->operator != DIV && test->right->right->operator != MINUS && test->right->right->operator != PLUS){
-					//printf("%c", buf[i]);
-					test->right->right = (struct expression*) malloc(sizeof(struct expression));
-					test->right->right->operator = buf[i];
-					test->right->right->type = 'P';
-					level = 3;
-					
-				}
-				else{
-					//printf("%c", buf[i]);
-					test->right->right->right = (struct expression*) malloc(sizeof(struct expression));
-					test->right->right->right->operator = buf[i];
-					test->right->right->right->type = 'P';
-					level = 4;
-					
-				}
-				
-			}
-			
-		}
-		else if(isalnum(buf[i])){
-			
-			int static fullLevel = 0;
-			
-			//decimal or identifier
-			//add on the current level
-			if(level == 4 && fullLevel != 4){
-				
-				if(!isdigit(test->right->right->right->right->value)){
-					
-					test->right->right->right->right->value = buf[i];
-					test->right->right->right->right->type = 'D';
-					
-				}
-				else if( !isdigit(test->right->right->right->left->value) ){
-					
-					test->right->right->right->left = (struct expression*) malloc(sizeof(struct expression));
-					test->right->right->right->left->value = buf[i];
-					test->right->right->right->left->type = 'D';
-					fullLevel = 4;
-					
-					
-					test->right->right->right->right->right->type = END_OF_FILE;
-					test->right->right->right->right->left->type = END_OF_FILE;
-					
-				}
-				
-			}
-			else if(fullLevel == 4 && isdigit(buf[i])){
-				
-				test->right->right->left = (struct expression*) malloc(sizeof(struct expression));
-				test->right->right->left->value = buf[i];
-				test->right->right->left->type = 'D';
-				
-				fullLevel = 3;
-				
-				test->right->right->left->right->type = END_OF_FILE;
-				test->right->right->left->left->type = END_OF_FILE;
-				
-			}
-			else if(fullLevel == 3){
-				
-				test->right->left = (struct expression*) malloc(sizeof(struct expression));
-				test->right->left->value = buf[i];
-				test->right->left->type = 'D';
-				
-				fullLevel = 2;
-				
-				test->right->left->left->type = END_OF_FILE;
-				test->right->left->right->type = END_OF_FILE;
-				
-			}
-			else if(fullLevel == 2){
-				
-				test->left = (struct expression*) malloc(sizeof(struct expression));
-				test->left->value = buf[i];
-				test->left->type = 'D';
-				
-				test->left->left->type = END_OF_FILE;
-				test->left->right->type = END_OF_FILE;
-				
-			}
-			
-		}
-		
-		i++;
-		
-	}
-	
-	//printTestTree(test);
-	
-	return buffer;
 	
 }
